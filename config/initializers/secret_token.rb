5995cae6c962ed4ad334747ea35871f47d642199
Jeff Cohen <jeff@codeacademy.org>
2012-10-30 21:20:34 -0500

    Start the app with one resource

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amdb::Application.config.secret_token = '27386406e7bfa9694761443da4c89a1567c969915c605b671d401594bd03cf30e81e215e5803e32944a9616ccc777ecb35bcfe0d57937fad87d78b994ed6b448'
